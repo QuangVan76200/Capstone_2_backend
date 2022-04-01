1100dce6d26fc4fd0c5a72b5abd76e863580ac11
Chung2308 <tranchung10a3@gmail.com>
2022-04-01 22:22:13 +0700

    Dai edit BE to Chung

--- src/app/models/DoctorModel.js
+++ src/app/models/DoctorModel.js
@@ -1,80 +1,88 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const plug = require('mongoose-slug-generator');
-
+const plug = require("mongoose-slug-generator");
 
 mongoose.plugin(plug);
 
-const Doctor = new Schema({
+const Doctor = new Schema(
+  {
     firstName: {
-        type: String,
-        default: ''
+      type: String,
+      default: "",
     },
     lastName: {
-        type: String,
-        default: ''
+      type: String,
+      default: "",
     },
     dateOfBirth: {
-        type: Date
+      type: Date,
     },
     phoneNumber: {
-        type: String,
-        minlength: 10,
-        maxlength: 10
+      type: String,
+      minlength: 10,
+      maxlength: 10,
     },
     gender: {
-        type: String,
-        enum:['Nam','Nữ','Giới Tính Thứ 3']
+      type: String,
+      enum: ["Nam", "Nữ", "Giới Tính Thứ 3"],
     },
     image: {
-        type: String,
-        default: ''
+      type: String,
+      default: "",
     },
     street: {
-        type: String,
-        default: ''
+      type: String,
+      default: "",
     },
     city: {
-        type: String,
-        default: ''
+      type: String,
+      default: "",
     },
     district: {
-        type: String,
-        default: ''
+      type: String,
+      default: "",
     },
     ward: {
-        type: String,
-        default: ''
+      type: String,
+      default: "",
     },
     zipCode: {
-        type: String,
-        default: '',
-        // maxlength:6,
-        // minlength:6
+      type: String,
+      default: "",
+      // maxlength:6,
+      // minlength:6
     },
     account: {
-        type: Schema.Types.ObjectId,
-        ref:'Accounts'
+      type: Schema.Types.ObjectId,
+      ref: "Accounts",
     },
-    review:[{
-        idOwner:{
-            type:mongoose.Types.ObjectId,
-            default:null,
-            ref:'owners'
+    review: [
+      {
+        idOwner: {
+          type: mongoose.Types.ObjectId,
+          default: null,
+          ref: "owners",
         },
-        rating:{
-            type:Number,
-            default:0,
-            min:0,
-            max:5,
+        rating: {
+          type: Number,
+          default: 0,
+          min: 0,
+          max: 5,
         },
-        comments:{
-            type:String,
-            default:'',
-        }
-    }]
-}, {
-    timestamps: true
-});
+        comments: {
+          type: String,
+          default: "",
+        },
+      },
+    ],
+    idSocket: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('Doctors', Doctor);
+module.exports = mongoose.model("Doctors", Doctor);
